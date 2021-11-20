2269f6b3cc0bb7c76384d6636e71c120d61024b3
root <root@Labtop-Henrik>
2021-11-20 11:39:58 +0100

    first commit

---
+++ test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+branches=$(git branch)
+if [ $? != 0 ]
+  then
+  exit
+fi
+
+branches=${branches//[* ]/}
+
+echo "branches:" 
+echo "$branches"
+
+changes=$(git diff --name-only)
+echo "$changes"
+
+if [ -z "$changes" ]
+  then
+  echo "${branches[0]} is will be bransh"
+fi
